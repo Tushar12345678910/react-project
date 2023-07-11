8c9ad5a6693024e686fdaa9a8bdd468b0d54addc
mayank <mayank23june@gmail.com>
2023-07-11 18:41:27 +0530

    all the basic functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(){
+    super();
+    console.warn("Hello from constructor");
+  }
+
+  componentDidMount()
+  {
+    console.warn("ComponentDidMount Method");
+  }
+  render(){
+    console.warn("render method");
+    return(
+      <div>
+        <h1>Life Cycle Method's Examples</h1>
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
+import Hello from './Hello';
+import StateExample from './StateExample';
+import PropsExample from './PropsExample';
+import GetInputFieldData from './GetInputFieldData';
+import ToggleExample from './ToggleExample';
+import IfElse from './IfElse';
+import Hooks from './Hooks';
+import ArrayExample from './ArrayExample';
 import reportWebVitals from './reportWebVitals';
+import RoutingExample from './RoutingExample';
+import DynamicRouting from './DynamicRouting';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <PropsExample name="tushar" email={"test@email.com"} others={{address:"Ujjain",age:"24"}} />
+    <br></br>
+    <GetInputFieldData/><br></br>
+    <ToggleExample/><br></br>
+    <IfElse/>
+    <Hooks/>
+    <ArrayExample/>
+    <App/> */}
+    <RoutingExample/>
+    <DynamicRouting />
   </React.StrictMode>
 );
 
